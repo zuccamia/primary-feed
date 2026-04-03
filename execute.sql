52fdfea6baf3ba32c11eee277cbdc3167b02ecb7
Hoang Nguyen Bich Ngoc <zuccamia@gmail.com>
2026-04-02 21:29:45 -0700

    Update with new table and file name.

--- execute.sql
+++ execute.sql
@@ -26,14 +26,15 @@ SELECT 'admin_permissions' AS table_name; SELECT * FROM admin_permissions;
 SELECT 'staff_admin_permissions' AS table_name; SELECT * FROM staff_admin_permissions;
 SELECT 'food_categories' AS table_name; SELECT * FROM food_categories;
 SELECT 'food_items' AS table_name; SELECT * FROM food_items;
-SELECT 'inventories' AS table_name; SELECT * FROM inventories;
 SELECT 'volunteer_shifts' AS table_name; SELECT * FROM volunteer_shifts;
 SELECT 'donors' AS table_name; SELECT * FROM donors;
 SELECT 'donations' AS table_name; SELECT * FROM donations;
 SELECT 'donation_items' AS table_name; SELECT * FROM donation_items;
+SELECT 'inventories' AS table_name; SELECT * FROM inventories;
 SELECT 'beneficiaries' AS table_name; SELECT * FROM beneficiaries;
 SELECT 'distributions' AS table_name; SELECT * FROM distributions;
 SELECT 'distribution_items' AS table_name; SELECT * FROM distribution_items;
+SELECT 'trigger_logs' AS table_name; SELECT * FROM trigger_logs;
 
 -- ───────────────────────────────────────
 -- Sample SQL queries
@@ -46,4 +47,4 @@ source dbSQL.sql;
 -- Sample procedure calls
 -- ───────────────────────────────────────
 SELECT 'Test calling procedures...' as message;
-source dbEXAMPLES.sql;
+source dbPROCSCALL.sql;
